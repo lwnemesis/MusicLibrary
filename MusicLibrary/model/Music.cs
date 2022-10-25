2fc5a645dabb68abde81dc7214ab26827c8e0d3e
qingliu816 <112495258+qingliu816@users.noreply.github.com>
2022-10-24 23:23:53 -0700

    A few initial changes
    1. Add gitignore file
    2. Add music model file
    3. Add button and menu in main page.

---
+++ MusicLibrary/model/Music.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicLibrary.model
+{
+    internal enum MusicCategory
+    { Pop,
+        Classic,
+        Rap,
+        Rock
+    }
+    internal class Music
+    {
+        public string Name { get; set; }//music name
+        public string AudioFile { get; set; }//music
+        public string ImageFile { get; set; } //maybe album cover
+        public MusicCategory Category { get; set; }
+        public Music(string name, MusicCategory category)
+        {
+            Name = name;
+            Category = category;
+            AudioFile = $"/Assets/Audio/{category}/{name}.wav";
+            ImageFile = $"/Assets/Images/{category}/{name}.png";
+        }
+    }
+}
