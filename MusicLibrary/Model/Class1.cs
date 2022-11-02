994ecbdf0d75c2a9e1c49e69899b5da3178175d9
qingliu816 <112495258+qingliu816@users.noreply.github.com>
2022-11-01 21:49:32 -0700

    update side menu

---
+++ MusicLibrary/Model/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicLibrary.Model
+{
+    internal class  MenuItem
+
+    {
+        public string IconFile { get; set; }
+        public MusicCategory Category { get; set; }
+
+    }
+}
