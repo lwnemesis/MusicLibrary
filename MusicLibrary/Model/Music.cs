46815fb0cefaf4bfeb4b7327c5d271518d632042
harshithavaas <harshitha.vaas@gmail.com>
2022-11-03 23:20:50 -0700

    - added more properties to music class like album, artist etc

--- MusicLibrary/Model/Music.cs
+++ MusicLibrary/Model/Music.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,19 +13,27 @@ namespace MusicLibrary.Model
         Classic,
         Rap,
         Rock
-    }
+    }  
     internal class Music
     {
         public string Name { get; set; }//music name
         public string AudioFile { get; set; }//music
         public string ImageFile { get; set; } //maybe album cover
+        public string Artist { get; set; }
+        public string Year { get; set; }
+        public string Album { get; set; }
         public MusicCategory Category { get; set; }
-        public Music(string name, MusicCategory category)
+
+        public Music(DataRow dr)
         {
-            Name = name;
-            Category = category;
-            AudioFile = $"/Assets/Audio/{category}/{name}.mp3";
-            ImageFile = $"/Assets/Images/{category}/{name}.png";
+            Name = dr["Name"].ToString();
+            Category = (MusicCategory)Enum.Parse(typeof(MusicCategory), dr["Category"].ToString());
+            Artist = dr["Artist"].ToString();
+            Year = dr["Year"].ToString();
+            Album = dr["Album"].ToString();
+            AudioFile = $"/Assets/Audio/{Category}/{Name}.mp3";
+            ImageFile = $"/Assets/Images/{Category}/{Name}.png";
+                     
         }
     }
 }
